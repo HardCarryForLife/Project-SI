6cf17e657d2438e6b1486c35bcddc53bef1a3133
Andi Afwan Ali Damopolii <afwanonepiece02@gmail.com>
2021-12-12 21:13:31 +0800

    Update + Delete

---
+++ stockbarang.sql
@@ -0,0 +1,166 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 11, 2021 at 08:14 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 7.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `stockbarang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keluar`
+--
+
+CREATE TABLE `keluar` (
+  `idkeluar` int(11) NOT NULL,
+  `idbarang` int(11) NOT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp(),
+  `penerima` varchar(50) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `keluar`
+--
+
+INSERT INTO `keluar` (`idkeluar`, `idbarang`, `tanggal`, `penerima`, `qty`) VALUES
+(1, 4, '2021-12-11 16:05:34', 'zaki', 16);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `iduser` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`iduser`, `email`, `password`) VALUES
+(1, 'sistem@gmail.com', '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `masuk`
+--
+
+CREATE TABLE `masuk` (
+  `idmasuk` int(11) NOT NULL,
+  `idbarang` int(11) NOT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp(),
+  `keterangan` varchar(50) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `masuk`
+--
+
+INSERT INTO `masuk` (`idmasuk`, `idbarang`, `tanggal`, `keterangan`, `qty`) VALUES
+(2, 4, '2021-12-11 15:49:55', 'zaki', 1),
+(3, 4, '2021-12-11 15:55:20', 'asep', 20);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stock`
+--
+
+CREATE TABLE `stock` (
+  `idbarang` int(11) NOT NULL,
+  `namabarang` varchar(50) NOT NULL,
+  `deskripsi` varchar(50) NOT NULL,
+  `stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `stock`
+--
+
+INSERT INTO `stock` (`idbarang`, `namabarang`, `deskripsi`, `stock`) VALUES
+(1, 'j1', 'hape', 1),
+(4, '13 pro', 'iphone', 5);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `keluar`
+--
+ALTER TABLE `keluar`
+  ADD PRIMARY KEY (`idkeluar`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`iduser`);
+
+--
+-- Indexes for table `masuk`
+--
+ALTER TABLE `masuk`
+  ADD PRIMARY KEY (`idmasuk`);
+
+--
+-- Indexes for table `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`idbarang`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `keluar`
+--
+ALTER TABLE `keluar`
+  MODIFY `idkeluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `iduser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `masuk`
+--
+ALTER TABLE `masuk`
+  MODIFY `idmasuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `stock`
+--
+ALTER TABLE `stock`
+  MODIFY `idbarang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
